d86e3d7b922c195420526a588873f5d4f3298453
paras-bst <91452776+paras-bst@users.noreply.github.com>
2026-01-08 17:17:05 +0530

    adding favicon and other fixes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let lastScrollY = window.scrollY;
+const header = document.querySelector("header");
+
 function scrollToProducts() {
   document.getElementById("products").scrollIntoView({ behavior: "smooth" });
 }
@@ -8,3 +11,23 @@ function handleSubmit(event) {
     "Thank you! Your message has been sent 🎉";
   return false;
 }
+
+window.addEventListener("scroll", () => {
+  const currentScrollY = window.scrollY;
+
+  // Shadow + logo shrink
+  if (currentScrollY > 20) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+
+  // Hide on scroll down, show on scroll up
+  if (currentScrollY > lastScrollY && currentScrollY > 120) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScrollY = currentScrollY;
+});
