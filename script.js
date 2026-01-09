1ace53f54d31f3d1d7fd4251054025a2420906eb
paras-bst <91452776+paras-bst@users.noreply.github.com>
2026-01-09 17:30:22 +0530

    banner on footer

--- script.js
+++ script.js
@@ -77,3 +77,15 @@ function updateArrows() {
 
 productGrid.addEventListener("scroll", updateArrows);
 updateArrows();
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+});
+
+reveals.forEach(r => observer.observe(r));
